14174956072865c34014abf1998b1afd6b0c4bd6
Aryan <69583042+aryannxroot@users.noreply.github.com>
2024-06-13 09:25:56 +0530

    learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -51,6 +51,23 @@ class Clothing extends Product {
   }
 }
 
+// const date = new Date();
+// console.log(date);
+// console.log(date.toLocaleTimeString());
+
+// console.log(this);
+
+// const object2 = {
+//   a: 2,
+//   b:this.a
+// };
+
+// function logThis(){
+//   console.log(this);
+// }
+// logThis();
+// logThis().call('hello');
+
 
 export const products = [
   {
